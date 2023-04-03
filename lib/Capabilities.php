89413daa1e37c909b4912be8c8a2c8de0d021caa
Eric <emuigai5@gmail.com>
2023-04-03 14:55:32 +0300

    fix shop permissions

--- lib/Capabilities.php
+++ lib/Capabilities.php
@@ -5,19 +5,22 @@ namespace Lasntg\Admin\Products;
 /**
  * Plugin capabilities assigned to roles
  */
-class Capabilities {
+class Capabilities
+{
 
 	/**
 	 * Admin capabilities
 	 */
-	public static function get_administrator_capabilities(): array {
+	public static function get_administrator_capabilities(): array
+	{
 		return array();
 	}
 
 	/**
 	 * National manager capabilities
 	 */
-	public static function get_national_manager_capabilities(): array {
+	public static function get_national_manager_capabilities(): array
+	{
 		return array(
 			'view_admin_dashboard',
 			'read',
@@ -52,7 +55,8 @@ class Capabilities {
 	/**
 	 * Regional training officer capabilities
 	 */
-	public static function get_regional_training_centre_manager_capabilities(): array {
+	public static function get_regional_training_centre_manager_capabilities(): array
+	{
 		return [
 			'view_admin_dashboard',
 			'read',
@@ -88,112 +92,118 @@ class Capabilities {
 	/**
 	 * Training officer capabilities
 	 */
-	public static function get_training_officer_capabilities(): array {
+	public static function get_training_officer_capabilities(): array
+	{
 		return [
 			'view_admin_dashboard',
 			'read',
+			'read_product',
+			'read_private_products',
+			'edit_others_products',
 		];
 	}
 
 	/**
 	 * Fire training officer capabilities
 	 */
-	public static function get_fire_training_officer_capabilities(): array {
+	public static function get_fire_training_officer_capabilities(): array
+	{
 		return [];
 	}
 
-	public static function add(): void {
-		$role = get_role( 'administrator' );
+	public static function add(): void
+	{
+		$role = get_role('administrator');
 		$caps = self::get_administrator_capabilities();
 		array_walk(
 			$caps,
-			function( $cap ) use ( $role ) {
-				$role->add_cap( $cap );
+			function ($cap) use ($role) {
+				$role->add_cap($cap);
 			}
 		);
 
-		$role = get_role( 'national_manager' );
+		$role = get_role('national_manager');
 		$caps = self::get_national_manager_capabilities();
 		array_walk(
 			$caps,
-			function( $cap ) use ( $role ) {
-				$role->add_cap( $cap );
+			function ($cap) use ($role) {
+				$role->add_cap($cap);
 			}
 		);
 
-		$role = get_role( 'regional_training_centre_manager' );
+		$role = get_role('regional_training_centre_manager');
 		$caps = self::get_regional_training_centre_manager_capabilities();
 		array_walk(
 			$caps,
-			function( $cap ) use ( $role ) {
-				$role->add_cap( $cap );
+			function ($cap) use ($role) {
+				$role->add_cap($cap);
 			}
 		);
 
-		$role = get_role( 'training_officer' );
+		$role = get_role('training_officer');
 		$caps = self::get_training_officer_capabilities();
 		array_walk(
 			$caps,
-			function( $cap ) use ( $role ) {
-				$role->add_cap( $cap );
+			function ($cap) use ($role) {
+				$role->add_cap($cap);
 			}
 		);
 
-		$role = get_role( 'fire_training_officer' );
+		$role = get_role('fire_training_officer');
 		$caps = self::get_fire_training_officer_capabilities();
 		array_walk(
 			$caps,
-			function( $cap ) use ( $role ) {
-				$role->add_cap( $cap );
+			function ($cap) use ($role) {
+				$role->add_cap($cap);
 			}
 		);
 	}
 
-	public static function remove(): void {
-		$role = get_role( 'administrator' );
+	public static function remove(): void
+	{
+		$role = get_role('administrator');
 		$caps = self::get_administrator_capabilities();
 		array_walk(
 			$caps,
-			function( $cap ) use ( $role ) {
-				$role->remove_cap( $cap );
+			function ($cap) use ($role) {
+				$role->remove_cap($cap);
 			}
 		);
 
-		$role = get_role( 'national_manager' );
+		$role = get_role('national_manager');
 		$caps = self::get_national_manager_capabilities();
 		array_walk(
 			$caps,
-			function( $cap ) use ( $role ) {
-				$role->remove_cap( $cap );
+			function ($cap) use ($role) {
+				$role->remove_cap($cap);
 			}
 		);
 
-		$role = get_role( 'regional_training_centre_manager' );
+		$role = get_role('regional_training_centre_manager');
 		$caps = self::get_regional_training_centre_manager_capabilities();
 		array_walk(
 			$caps,
-			function( $cap ) use ( $role ) {
-				$role->remove_cap( $cap );
+			function ($cap) use ($role) {
+				$role->remove_cap($cap);
 			}
 		);
 
-		$role = get_role( 'training_officer' );
+		$role = get_role('training_officer');
 		$caps = self::get_training_officer_capabilities();
 		array_walk(
 			$caps,
-			function( $cap ) use ( $role ) {
-				$role->remove_cap( $cap );
+			function ($cap) use ($role) {
+				$role->remove_cap($cap);
 			}
 		);
 
-		$role = get_role( 'fire_training_officer' );
+		$role = get_role('fire_training_officer');
 		$caps = self::get_fire_training_officer_capabilities();
 		array_walk(
 			$caps,
-			function( $cap ) use ( $role ) {
-				$role->remove_cap( $cap );
+			function ($cap) use ($role) {
+				$role->remove_cap($cap);
 			}
 		);
 	}
-
 }

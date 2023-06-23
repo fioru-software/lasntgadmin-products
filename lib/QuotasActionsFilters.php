f61f8cc35185ff5b60a12faa8de469e5816a5af9
Eric <emuigai5@gmail.com>
2023-06-23 11:44:43 +0300

    updates

--- lib/QuotasActionsFilters.php
+++ lib/QuotasActionsFilters.php
@@ -210,7 +210,9 @@ class QuotasActionsFilters {
 			$field     = sanitize_text_field( wp_unslash( $_POST[ '_quotas_field_' . $group->group_id ] ) ); // phpcs:ignore WordPress.Security.NonceVerification.Missing
 			$old_value = get_post_meta( $post_id, '_quotas_field_' . $group->group_id, true );
 			update_post_meta( $post_id, '_quotas_field_' . $group->group_id, esc_attr( $field ) );
-			do_action( 'lasntgadmin-products_quotas_field_changed', $post_id, $group->group_id, $old_value, $field );
+			if($old_value != $field){
+				do_action( 'lasntgadmin-products_quotas_field_changed', $post_id, $group->group_id, $old_value, $field );
+			}
 		}
 
 		if ( isset( $_POST['_minimum_capacity'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing

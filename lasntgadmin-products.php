35592930464e85dd95544fc3d5b1243d8b7053e0
Eric <emuigai5@gmail.com>
2023-10-25 13:56:38 +0300

    updates

--- lasntgadmin-products.php
+++ lasntgadmin-products.php
@@ -3,7 +3,7 @@
  * Plugin Name:       LASNTG Products
  * Plugin URI:        https://github.com/fioru-software/lasntgadmin-products
  * Description:       Lasntg Products
- * Version:           5.12.1-rc
+ * Version:           6.0.2-rc1
  * Requires PHP:      7.2
  * Text Domain:       lasntgadmin
  * Domain Path:       /languages

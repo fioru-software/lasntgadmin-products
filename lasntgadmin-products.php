c9cb759039d562c984524547ea75f51c5ff24547
cm <7zh6dj9f9@mozmail.com>
2023-11-02 09:37:30 +0000

    patch ver inc

--- lasntgadmin-products.php
+++ lasntgadmin-products.php
@@ -3,7 +3,7 @@
  * Plugin Name:       LASNTG Products
  * Plugin URI:        https://github.com/fioru-software/lasntgadmin-products
  * Description:       Lasntg Products
- * Version:           5.12.1
+ * Version:           5.12.2-rc1
  * Requires PHP:      7.2
  * Text Domain:       lasntgadmin
  * Domain Path:       /languages

4ba05561641908fa4dfce4b5177ce9b0c69cbebf
cm <7zh6dj9f9@mozmail.com>
2023-12-08 12:59:50 +0000

    made groups a dep

--- scripts/caps.sh
+++ scripts/caps.sh
@@ -5,6 +5,6 @@ dir="/usr/local/src/exports/caps/*.txt"
 for role_file in $dir; do
     role_name=$(basename $role_file .txt)
     while read -r cap; do
-        runuser -s /bin/sh -c "wp cap add $role_name $cap" www-data
+        runuser -s /bin/sh -c "wp cap add $role_name $cap --path=/var/www/html" www-data
     done <$role_file
 done;

c25cce9ff55440e184cfec4c09b9c79aa47cc1ea
Eric <emuigai5@gmail.com>
2024-04-11 12:06:47 +0300

    make sure it's unique

--- lib/ProductSchedulerActions.php
+++ lib/ProductSchedulerActions.php
@@ -157,9 +157,9 @@ class ProductSchedulerActions {
 	public static function queue_async_action() {
 		if ( function_exists( 'as_has_scheduled_action' ) ) {
 			if ( false === as_has_scheduled_action( 'lasntgadmin_close_courses' ) ) {
-				as_schedule_recurring_action( strtotime( '+1 hour' ), HOUR_IN_SECONDS, 'lasntgadmin_close_courses' );
+				as_schedule_recurring_action( strtotime( '+1 hour' ), HOUR_IN_SECONDS, 'lasntgadmin_close_courses', [], '', true );
 			}if ( false === as_has_scheduled_action( 'lasntgadmin_close_notify' ) ) {
-				as_schedule_recurring_action( strtotime( '+1 hour' ), HOUR_IN_SECONDS, 'lasntgadmin_close_notify' );
+				as_schedule_recurring_action( strtotime( '+1 hour' ), HOUR_IN_SECONDS, 'lasntgadmin_close_notify', [], '', true );
 			}
 		}
 	}

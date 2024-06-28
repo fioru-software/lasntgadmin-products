803426f575f096da1130144b90c53377478bd772
cm <7zh6dj9f9@mozmail.com>
2024-06-28 15:35:27 +0100

    rc release

--- lasntgadmin-products.php
+++ lasntgadmin-products.php
@@ -3,7 +3,7 @@
  * Plugin Name:       LASNTG Products
  * Plugin URI:        https://github.com/fioru-software/lasntgadmin-products
  * Description:       Lasntg Products
- * Version:           5.14.9
+ * Version:           5.14.10-rc1
  * Requires PHP:      7.2
  * Text Domain:       lasntgadmin
  * Domain Path:       /languages

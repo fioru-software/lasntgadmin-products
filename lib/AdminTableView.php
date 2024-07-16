72d8ffe182aa6408b8cb9ea53cd1ff5db80e7f8e
Eric <emuigai5@gmail.com>
2024-07-16 10:34:56 +0300

    hide unwanted status

--- lib/AdminTableView.php
+++ lib/AdminTableView.php
@@ -73,7 +73,10 @@ class AdminTableView {
 
 	public static function add_columns_css( $defaults ) {
 		$assets_dir = untrailingslashit( plugin_dir_url( __FILE__ ) ) . '/../assets/';
-		wp_enqueue_style( 'admin-columns', $assets_dir . 'styles/admin-column.css', [], '1.1.1' );
+		wp_enqueue_style( 'admin-columns', $assets_dir . 'styles/admin-column.css', [], PluginUtils::get_version() );
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_enqueue_style( 'non-admin-table-view', $assets_dir . 'styles/non-admin-table-view.css', [], 123 );
+		}
 		return $defaults;
 	}
 

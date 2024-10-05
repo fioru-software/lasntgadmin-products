c2efef7c4b4ddf78ef127f45c22cf3995b6fed40
Eric <emuigai5@gmail.com>
2024-10-05 11:32:41 +0300

    bump version

--- lasntgadmin-products.php
+++ lasntgadmin-products.php
@@ -3,7 +3,7 @@
  * Plugin Name:       LASNTG Products
  * Plugin URI:        https://github.com/fioru-software/lasntgadmin-products
  * Description:       Lasntg Products
- * Version:           5.14.19-rc1
+ * Version:           5.14.19
  * Requires PHP:      7.2
  * Text Domain:       lasntgadmin
  * Domain Path:       /languages
